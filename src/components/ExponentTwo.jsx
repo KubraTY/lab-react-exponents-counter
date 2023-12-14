6876bf0b45e545a6ca872976a23afecdbcc9e6b3
Kubra Tokgozlu <kubra.tokgozlu@gmail.com>
2023-12-14 12:32:52 +0100

    week4-day4-exponents-counter

--- src/components/ExponentTwo.jsx
+++ src/components/ExponentTwo.jsx
@@ -1,8 +1,12 @@
-const ExponentTwo = () => (
-   <div className="exponent-counter-container">
-    <p className="exponent-label">n²</p>
-    <p className="exponent-result">2 * 2 = <span className="total">4</span></p>
-  </div>
-);
+const ExponentTwo = ({count}) => {
+    const total = count * count
+    return (
+      <div className="exponent-counter-container">
+      <p className="exponent-label">n²</p>
+      <p className="exponent-result">{count} * {count} = <span className="total">{total}</span></p>
+    </div>
+    )
+ 
+    };
 
 export default ExponentTwo;
